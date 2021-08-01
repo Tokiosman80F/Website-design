51e761f0f8d23bfb9d769160541ea5efd5ef9514
Tokiosman80F <Tokiosman80F@users.noreply.github.com>
2021-08-01 20:35:26 +0600

    working on slick slider

---
+++ Creatice-Agency/js/script.js
@@ -0,0 +1,15 @@
+
+$(document).ready(function(){
+    
+  
+$('.banner-slider').slick({
+    infinite: true,
+    autoplaySpeed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows:false,
+    dots:true
+  });
+
+
+  });
