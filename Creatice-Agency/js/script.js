a15a32f873470fe79e9499383d8842c392ba4213
Tokiosman80F <Tokiosman80F@users.noreply.github.com>
2021-08-05 23:57:09 +0600

    slick slide perfected

--- Creatice-Agency/js/script.js
+++ Creatice-Agency/js/script.js
@@ -4,11 +4,13 @@ $(document).ready(function(){
   
 $('.banner-slider').slick({
     infinite: true,
-    autoplaySpeed: 500,
+    autoplaySpeed: 1000,
     slidesToShow: 1,
     slidesToScroll: 1,
     arrows:false,
-    dots:true
+    dots:true,
+    fade:true,
+    autoplay:true,
   });
 
 
